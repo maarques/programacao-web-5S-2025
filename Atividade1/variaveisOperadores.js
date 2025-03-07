ed96efb4360f689ab606b1b551e64a8d76f0be97
maarques <147277362+maarques@users.noreply.github.com>
2025-03-07 09:35:41 -0300

    Atividade1

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+let num1 = 24;
+let num2 = 12;
+
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+console.log(`Resultado da soma entre ${num1} e ${num2} é ${soma}`);
+console.log(`Resultado da subtração entre ${num1} e ${num2} é ${sub}`);
+console.log(`Resultado da multiplicação entre ${num1} e ${num2} é ${mult}`);
+console.log(`Resultado da divisão entre ${num1} e ${num2} é ${div}`);
