cab158dbcf99e759b76be9ac3286d736f042e9f4
maarques <thi4go.marqu3s@gmail.com>
2025-03-13 09:31:17 -0300

    add atividade2

---
+++ Atividade2/matrizTransposta.js
@@ -0,0 +1,29 @@
+function transporMatriz(m){
+    for(let i = 0; i < m.length; j++){
+        let linha = "";
+        
+        for(let j = 0; j < m[i].length; j++){
+            linha += m[i][j] + "\t";
+
+        }
+        console.log(linha);
+    }
+
+    console.log('\nTransposta\n');
+
+    for(let j = 0; j < m[0].length; j++){
+        let linha = "";
+        for(let i = 0; i < m.length; i++){
+            linha += m[i][j] + "\t";
+        }
+        console.log(linha);
+    }
+}
+
+let matriz = [
+    [1,2],
+    [3,4],
+    [5,6]
+]
+
+transporMatriz(m);
