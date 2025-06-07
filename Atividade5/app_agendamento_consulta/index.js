2798d5a4024455e77d1b76483567dbeeebb450bb
maarques <thi4go.marqu3s@gmail.com>
2025-06-07 18:12:02 -0300

    app agendamentos finalizado

--- Atividade5/app_agendamento_consulta/index.js
+++ Atividade5/app_agendamento_consulta/index.js
@@ -1,5 +1,6 @@
-const express = require("express");
+const express = require('express');
 const mustacheExpress = require('mustache-express');
+const db  = require('./db');
 const app = express();
 
 app.engine('html', mustacheExpress());
@@ -7,34 +8,12 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res)=>{
-    res.render('index.html');
-});
+const agendamentoRouter = require('./routers/agendamentoRouter');
+app.use('/', agendamentoRouter);
 
-app.post('/agendar_consulta', (req, res)=>{
-    let dados_consulta = req.body;
-    let erro_form = false;
-    let campos_invalidos = [];
-
-    if(dados_consulta.nome.length == 0){
-        erro_form = true;
-        campos_invalidos.push("Nome");
-    }
-
-    if(dados_consulta.sobrenome.length == 0){
-        erro_form = true;
-        campos_invalidos.push("Sobrenome");
-    }
-
-    if(dados_consulta.cpf.length == 0){
-        erro_form = true;
-        campos_invalidos.push("CPF");
-    }
-
-    res.render('index.html', {erro_form, dados_consulta, campos_invalidos});
-});
+db.sync();
 
 const PORT = 8080;
 app.listen(PORT, ()=>{
-    console.log("app rodando na porta: " + PORT);
+    console.log('app rodando na porta ' + PORT);
 });
